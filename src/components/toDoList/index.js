36f140c24668279296a5ac83fd36606675b4c151
Marcin Lautsch <mldemigod@gmail.com>
2022-10-23 16:14:12 +0200

    ToDoList_02

--- src/components/toDoList/index.js
+++ src/components/toDoList/index.js
@@ -3,9 +3,13 @@ import { v4 as uuidv4 } from 'uuid'
 
 // 1. Stworz formularz, ktory posiada pole title i guzik do submitowania formularza. (aby dodac zadanie do listy)
 // 2. Stworz listę, która będzie przetrzymywała zadanie do wyswietlenia (tylko jedno)
-// 3*. przy uzyciu funkcji map, spraw zeby mozna bylo dodawac wiele zadan
-// 4**. dodaj hook useEffect i dane odczytuj i zapisuj do localStorage :)
-// 5***. dodaj json-server i zrob obsluge API
+// 3. przy uzyciu funkcji map, spraw zeby mozna bylo dodawac wiele zadan
+// 4. dodaj hook useEffect i dane odczytuj i zapisuj do localStorage :)
+
+// Zadanie domowe
+
+// 5*. Zrob obsluge endpointa https://jsonplaceholder.typicode.com/todos do pobierania TODOS
+// 6*. dodaj json-server i zrob obsluge API
 
 function TodoList() {
 	const [todoInputValue, setTodoInputValue] = useState('')
@@ -22,13 +26,18 @@ function TodoList() {
 	// Opis
 	// pusta tablica [] oznacza, ze funkcja odpali tylko tylko raz, po pierwszym renderze
 
+	// useEffect(() => {
+	//   getTime(); // pierwsze wywolanie (zeby nie bylo opoznienia)
+	//   // setInterval odpala sie co sekunde
+	//   setInterval(() => {
+	//     // co sekunde update czasu
+	//     getTime();
+	//   }, 1000)
+	// }, [])
+
 	useEffect(() => {
-		getTime() // pierwsze wywolanie (zeby nie bylo opoznienia)
-		// setInterval odpala sie co sekunde
-		setInterval(() => {
-			// co sekunde update czasu
-			getTime()
-		}, 1000)
+		const todos = JSON.parse(localStorage.getItem('todos')) ?? []
+		setTodos(todos)
 	}, [])
 
 	const getTime = () => {
@@ -41,25 +50,34 @@ function TodoList() {
 		setTodoInputValue(event.target.value)
 	}
 
+	// 1. przy dodawaniu nowego todo, zapisz jego date stworzenia pod kluczem createdAt
+	// 2. Wyswietl date o ktorej task zostal dodany do listy przy kazdym zadaniu
+	// 3. Jesli pole createdAt jest puste, to nie wyswietlaj zadnego tagu HTML
+
 	const handleSubmit = event => {
 		// MUST HAVE PRZY SUBMICIE
 		event.preventDefault()
 
 		// setTodoText(todoInputValue)
 
+		const dateAdded = new Date()
+
 		const newTodo = {
 			id: uuidv4(),
 			title: todoInputValue,
+			createdAt: dateAdded.toString(),
 		}
 
 		const newTodos = todos.concat(newTodo)
 		setTodos(newTodos)
+		localStorage.setItem('todos', JSON.stringify(newTodos))
 
 		setTodoInputValue('')
 	}
 
 	const handleRemoveTodos = () => {
 		setTodos([])
+		localStorage.removeItem('todos')
 	}
 
 	return (
@@ -76,16 +94,13 @@ function TodoList() {
 				<button type='submit'>Add Task</button>
 			</form>
 			<ul>
-				{/* <li>{todoText}</li> */}
-
-				{/* {
-          title: 'Wynieść śmieci'
-        } */}
-
-				{/* <li>{TODOS[0].title}</li> */}
-
 				{todos.map(todo => {
-					return <li key={todo.id}>{todo.title}</li>
+					return (
+						<li key={todo.id}>
+							{todo.title}
+							{todo.createdAt ? <span>| {todo.createdAt} </span> : null}
+						</li>
+					)
 				})}
 			</ul>
 			{/* 2. Zrobic obsluge usuwania zadan z listy */}
